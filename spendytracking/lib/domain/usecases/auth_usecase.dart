ed5b4c4797629ac341ce47ae8ba7564d9b374743
htmn <nghia.htm97@gmail.com>
2025-01-27 12:24:26 +0700

    update user model

--- spendytracking/lib/domain/usecases/auth_usecase.dart
+++ spendytracking/lib/domain/usecases/auth_usecase.dart
@@ -1,14 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:spendytracking/data/models/user_model.dart';
 import 'package:spendytracking/data/repositories/auth_repository.dart';
+import 'package:spendytracking/utils/extensions/user_firebase_extension.dart';
 
 class AuthUseCase {
   final AuthRepository authRepository;
 
   AuthUseCase({required this.authRepository});
 
+  UserModel? userModel;
+
+  Future<UserModel?> getUser()async{
+    final user = await authRepository.login();
+    return userModel = user?.userModel;
+  }
+
   Future<String?> checkLoginSuccess() async {
     try {
-      final user = await authRepository.login();
+      final user = await getUser();
       if (user == null) {
         return "Not Found User";
       }
