a761b2b857a668a75a2065ba76caa8d1c72e6e2b
htmn <nghia.htm97@gmail.com>
2025-02-07 14:43:53 +0700

    bloc test

--- spendytracking/macos/Flutter/GeneratedPluginRegistrant.swift
+++ spendytracking/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import firebase_core
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
