a651e6ac22c4b37582bcbcb11ea6972059e185d2
htmn <nghia.htm97@gmail.com>
2025-02-14 23:04:32 +0700

    git

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "🚀 Bắt đầu quá trình Build & Deploy tới thiết bị test..."
+
+echo "📌 Cài đặt dependencies..."
+flutter pub get
+
+echo "📌 Chạy unit test..."
+flutter test
+
+# 📱 Build Android & upload lên Firebase
+echo "📌 Build APK (Android)..."
+flutter build apk --release
+echo "📌 Upload APK lên Firebase App Distribution..."
+firebase appdistribution:distribute build/app/outputs/apk/debug/app-release.apk \
+  --app $FIREBASE_APP_ID_ANDROID \
+  --token "$FIREBASE_TOKEN" \
+  --groups test-1
+
+# 🍏 Build iOS & upload lên TestFlight
+#echo "📌 Build iOS..."
+#flutter build ios --no-codesign
+#echo "📌 Cài đặt Fastlane..."
+#gem install fastlane -NV
+#echo "📌 Upload lên TestFlight..."
+#cd ios && fastlane deploy_to_testflight
